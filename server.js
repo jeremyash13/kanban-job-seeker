0f2e71b48fd64d07ec4fd2ecc46a4e7175c6a3c1
Jeremy <jeremyash13@gmail.com>
2020-08-19 19:54:56 -0600

    setup server

---
+++ server.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 4000;
+
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+// Connection URI
+const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.ftljt.mongodb.net/seekr?retryWrites=true&w=majority`;
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect().then(() => {
+      app.options("*", cors());
+      app.use(
+        cors({
+          origin: "*",
+          methods: "GET,PUT,POST,DELETE",
+        })
+      );
+      //   app.use(bodyParser.urlencoded({ extended: true }));
+      //   app.use(bodyParser.json());
+
+      app.get("/items", async (req, res) => {
+        try {
+          const database = client.db("seekr");
+          const collection = database.collection("items");
+
+          const findResult = await collection
+            .find({
+              user: "john",
+            })
+            .toArray();
+          res.json(findResult);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+
+      app.listen(port, () => {
+        console.log(`listening at http://localhost:${port}`);
+      });
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
