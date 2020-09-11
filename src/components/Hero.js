e78b7352aa2958835939d1f4dd16080f676c844c
Jeremy <jeremyash13@gmail.com>
2020-09-10 20:59:39 -0600

    fixed bug with hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
     <div className="hero-img relative">
       <SimpleSeekLogoLanding />
       <div className="hero-overlay absolute inset-0"></div>
-      <div className="text-white hero-text-alignment w-full px-4 lg:max-w-screen-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row lg:justify-between">
+      <div className="text-white hero-text-alignment w-full px-4 lg:max-w-screen-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row lg:justify-between">
         <div className="lg:max-w-lg my-auto pr-4">
           <h3 className="text-white">Welcome to Simple Seek.</h3>
           <p>
