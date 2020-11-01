ffcb5330da607681bf8c453a66a8b34b5a1b74d5
Jeremy <jeremyash13@gmail.com>
2020-11-01 14:17:54 -0700

    server.js is a clone of api/app.js to locally test the API for the site.

--- server.js
+++ server.js
@@ -1,57 +1,198 @@
-require("dotenv").config();
-const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 4000;
+// THIS FILE IS NOT USED FOR PRODUCTION
+// SAVE FOR LOCALLY TESTING THE API UNTIL I CAN GET AWS'S SAM CLI TO RUN THE SERVERLESS FUNCTIONS
 
-const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+
+// require("dotenv").config();
+// const express = require("express");
+// const { MongoClient, ObjectId } = require("mongodb");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+// const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+// // Connection URI
+// const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.ftljt.mongodb.net/seekr?retryWrites=true&w=majority`;
+
+// // Create a new MongoClient
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server
+//     await client.connect().then(() => {
+//       app.options("*", cors());
+//       app.use(
+//         cors({
+//           origin: "*",
+//           methods: "GET,PUT,POST,DELETE",
+//         })
+//       );
+//       app.use(bodyParser.urlencoded({ extended: true }));
+//       app.use(bodyParser.json());
+
+//       app.post("/items", async (req, res) => {
+//         try {
+//           const database = client.db("seekr");
+//           const collection = database.collection("items");
+
+//           const { user } = req.body;
+
+//           const findResult = await collection
+//             .find({
+//               user: user,
+//             })
+//             .toArray();
+//           res.json(findResult);
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       });
+//       app.post("/newitem", async (req, res) => {
+//         try {
+//           const { _id, user, role, company, status, note } = req.body;
+
+//           let newDoc = {
+//             _id: ObjectId(_id.toString()),
+//             user: user,
+//             role: role,
+//             company: company,
+//             status: status,
+//             note: note,
+//           };
+
+//           const database = client.db("seekr");
+//           const collection = database.collection("items");
+
+//           const result = await collection
+//             .insertOne(newDoc)
+//             .then(() => res.json({ message: "SUCCESSFUL PUT REQUEST" }));
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       });
+//       app.post("/updateitem", async (req, res) => {
+//         try {
+//           const { _id, user, role, company, status, note } = req.body;
+
+//           const database = client.db("seekr");
+//           const collection = database.collection("items");
+
+//           collection
+//             .updateOne(
+//               {
+//                 _id: ObjectId(_id),
+//               },
+//               {
+//                 $set: {
+//                   user: user,
+//                   role: role,
+//                   company: company,
+//                   status: status,
+//                   note: note,
+//                 },
+//               }
+//             )
+//             .then(() => res.json({ message: "SUCCESSFUL PUT REQUEST" }));
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       });
+//       app.post("/deleteitem", async (req, res) => {
+//         try {
+//           const { _id, user } = req.body;
+
+//           const database = client.db("seekr");
+//           const collection = database.collection("items");
+
+//           collection
+//             .deleteOne({
+//               _id: ObjectId(_id.toString()),
+//             })
+//             .then(() => res.json({ message: "SUCCESSFUL DELETE REQUEST" }));
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       });
+
+//       app.listen(port, () => {
+//         console.log(`listening at http://localhost:${port}`);
+//       });
+//     });
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// ---------------------------------------------------------------------------------------------
+// API/app.js (below)
+// ---------------------------------------------------------------------------------------------
+
+require("dotenv").config()
+const express = require("express")
+const { MongoClient, ObjectId } = require("mongodb")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const app = express()
+
+const port = 4000
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
 
 // Connection URI
-const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.ftljt.mongodb.net/seekr?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.ftljt.mongodb.net/retryWrites=true&w=majority`
 
 // Create a new MongoClient
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  useNewUrlParser: true,
+})
 
 async function run() {
   try {
-    // Connect the client to the server
     await client.connect().then(() => {
-      app.options("*", cors());
+      const database = client.db("seekr")
+      const collection = database.collection("items")
+
+      app.options("*", cors())
       app.use(
         cors({
           origin: "*",
           methods: "GET,PUT,POST,DELETE",
         })
-      );
-      app.use(bodyParser.urlencoded({ extended: true }));
-      app.use(bodyParser.json());
+      )
+
+      app.use(bodyParser.urlencoded({ extended: true }))
+      app.use(bodyParser.json())
 
       app.post("/items", async (req, res) => {
-        try {
-          const database = client.db("seekr");
-          const collection = database.collection("items");
 
-          const { user } = req.body;
+        const { user } = req.body
 
-          const findResult = await collection
-            .find({
-              user: user,
-            })
-            .toArray();
-          res.json(findResult);
-        } catch (err) {
-          console.log(err);
-        }
-      });
+        const findResult = await collection
+          .find({
+            user: user,
+          })
+          .toArray()
+        res.set({
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT",
+        })
+        res.json(findResult)
+      })
       app.post("/newitem", async (req, res) => {
         try {
-          const { _id, user, role, company, status, note } = req.body;
+          const { _id, user, role, company, status, note } = req.body
 
           let newDoc = {
             _id: ObjectId(_id.toString()),
@@ -60,24 +201,23 @@ async function run() {
             company: company,
             status: status,
             note: note,
-          };
+          }
 
-          const database = client.db("seekr");
-          const collection = database.collection("items");
-
-          const result = await collection
-            .insertOne(newDoc)
-            .then(() => res.json({ message: "SUCCESSFUL PUT REQUEST" }));
+          await collection.insertOne(newDoc).then(() => {
+            res.set({
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "Content-Type",
+              "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT",
+            })
+            res.json({ message: "SUCCESSFUL PUT REQUEST" })
+          })
         } catch (err) {
-          console.log(err);
+          console.log(err)
         }
-      });
+      })
       app.post("/updateitem", async (req, res) => {
         try {
-          const { _id, user, role, company, status, note } = req.body;
-
-          const database = client.db("seekr");
-          const collection = database.collection("items");
+          const { _id, user, role, company, status, note } = req.body
 
           collection
             .updateOne(
@@ -94,35 +234,47 @@ async function run() {
                 },
               }
             )
-            .then(() => res.json({ message: "SUCCESSFUL PUT REQUEST" }));
+            .then(() => {
+              res.set({
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT",
+              })
+              res.json({ message: "SUCCESSFUL PUT REQUEST" })
+            })
         } catch (err) {
-          console.log(err);
+          console.log(err)
         }
-      });
+      })
       app.post("/deleteitem", async (req, res) => {
         try {
-          const { _id, user } = req.body;
-
-          const database = client.db("seekr");
-          const collection = database.collection("items");
+          const { _id } = req.body
 
           collection
             .deleteOne({
               _id: ObjectId(_id.toString()),
             })
-            .then(() => res.json({ message: "SUCCESSFUL DELETE REQUEST" }));
+            .then(() => {
+              res.set({
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT",
+              })
+              res.json({ message: "SUCCESSFUL DELETE REQUEST" })
+            })
         } catch (err) {
-          console.log(err);
+          console.log(err)
         }
-      });
+      })
 
       app.listen(port, () => {
-        console.log(`listening at http://localhost:${port}`);
-      });
-    });
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+        console.log(`Listening @ http://localhost:${port}`)
+      })
+    })
+  } catch (err) {
+    console.log(err)
   }
 }
-run().catch(console.dir);
+run()
+
+module.exports = app
