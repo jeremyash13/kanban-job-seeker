37ae0a345213c654d2df8b6b93da7b83b3d4925b
Jeremy <jeremyash13@gmail.com>
2020-11-01 14:35:10 -0700

    added a check for if the  document object is available before reading cookies to solve gatsby build failing.

--- src/hooks/useCookie.js
+++ src/hooks/useCookie.js
@@ -1,28 +1,36 @@
 import { useState } from "react"
 import ObjectID from "bson-objectid"
 
+const isBrowser = typeof window !== `undefined`
+
 const checkCookie = () => {
-  return document.cookie.includes("userID=")
+  if (isBrowser) {
+    return document.cookie.includes("userID=")
+  }
 }
 
 const getUserId = () => {
-  const cookieStr = document.cookie.split(";")[0]
-  const userId = cookieStr.slice(cookieStr.indexOf("=") + 1, cookieStr.length)
-  return userId
+  if (isBrowser) {
+    const cookieStr = document.cookie.split(";")[0]
+    const userId = cookieStr.slice(cookieStr.indexOf("=") + 1, cookieStr.length)
+    return userId
+  }
 }
 
 const createCookie = () => {
-  const newUserId = ObjectID().toString()
-  document.cookie = `userID=${newUserId};expires=Sun, 31 Dec 6969 10:59:59 GMT;path=/`
+  if (isBrowser) {
+    const newUserId = ObjectID().toString()
+    document.cookie = `userID=${newUserId};expires=Sun, 31 Dec 6969 10:59:59 GMT;path=/`
+  }
 }
 
 export default function () {
   const isFound = checkCookie()
-  const newCookie = createCookie;
-  const getUser = getUserId;
+  const newCookie = createCookie
+  const getUser = getUserId
   return {
     isFound,
     newCookie,
-    getUser
+    getUser,
   }
 }
