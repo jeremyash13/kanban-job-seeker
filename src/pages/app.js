2c746df7a0a12ebd3cd560eb8839f0b682729358
Jeremy <jeremyash13@gmail.com>
2020-11-01 14:26:19 -0700

    app now reads userId from browser cookies

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,36 +1,60 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import DragNDrop from "../components/DragNDrop"
 import Navbar from "../components/Navbar"
 import { Fade } from "shards-react"
 import Global from "../state/Global"
 import SEO from "../components/seo"
-import { navigate } from "gatsby"
-import { useAuth } from "react-use-auth"
+
+// import { navigate } from "gatsby"
+// import { useAuth } from "react-use-auth"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "shards-ui/dist/css/shards.min.css"
 
-function App() {
-  const { isAuthenticated } = useAuth()
-  const isBrowser = typeof window !== `undefined`
+import useCookie from "../hooks/useCookie"
 
-  if (!isAuthenticated() && isBrowser) {
+function App() {
+  // const { isAuthenticated, user } = useAuth()
+  // const isBrowser = typeof window !== `undefined`
+  const { isFound, newCookie, getUser } = useCookie()
+  const [userId, setUserId] = useState(null)
+
+  useEffect(() => {
+    if (!isFound) {
+      // create cookie
+      newCookie()
+    }
+
+    setUserId(getUser())
+
+    // if (!isAuthenticated() && isBrowser) {
+    // if user is not authenticated
     // need to check if Window Object is defined when calling navigate() or build will fail
-    navigate("/")
-    return null
-  } else {
-    return (
-      <Global.Provider>
-        <Fade>
-          <SEO title="Board" />
-          <div className="p-4">
-            <Navbar />
-            <DragNDrop />
-          </div>
-        </Fade>
-      </Global.Provider>
-    )
-  }
+
+    // if (!isFound) {
+    // create cookie
+    // newCookie()
+    // }
+
+    // setUserId(getUser())
+    // }
+    // else {
+    //   // if user is authenticated
+
+    // }
+  }, [])
+
+  return (
+    <Global.Provider>
+      <Fade>
+        <SEO title="Board" />
+        <Navbar />
+        <div className="p-4">
+          <DragNDrop userId={userId} />
+        </div>
+      </Fade>
+    </Global.Provider>
+  )
 }
 
 export default App
