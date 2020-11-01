5d3c0927a8e08464dfdf5149132bb5afcbf2d2c3
Jeremy <jeremyash13@gmail.com>
2020-11-01 14:19:15 -0700

    call to action now links to the app page instead of auth0

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,17 +1,16 @@
+import { Link } from "gatsby"
 import React from "react"
 import { useAuth } from "react-use-auth"
 
 function CallToAction() {
-  const { login } = useAuth()
+  // const { login } = useAuth()
   return (
-    <div
+    <Link
+      to="/app"
       className="px-3 py-2 border-2 inline-block border-white rounded-full text-whitewhite text-decoration-none cursor-pointer hover:bg-white hover:text-primary transition-colors duration-300 ease-in-out"
-      onClick={() => {
-        login()
-      }}
     >
       Get Started
-    </div>
+    </Link>
   )
 }
 
